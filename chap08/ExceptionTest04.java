114ebf05804c15aa433d066c0e4cb47fe6520582
snowcuphea <snowcuphea@gmail.com>
2019-12-16 17:45:32 +0900

    finally added

--- chap08/ExceptionTest04.java
+++ chap08/ExceptionTest04.java
@@ -1,5 +1,6 @@
 package chap08;
-
+// 예외가 발생하거나 발생하지 않거나 반드시 실행할 명령문이 있는 경우?
+// finally블럭을 추가하고 정의한다.
 import java.util.Scanner;
 
 public class ExceptionTest04 {
@@ -15,12 +16,14 @@ public class ExceptionTest04 {
 			}
 			System.out.println(arr[1].length());
 			
-		}catch(ArrayIndexOutOfBoundsException e) {
+		}catch(ArrayIndexOutOfBoundsException e) {//e는 블럭안에 쓰인 지역변수라서 여러 개 써도됨
 			System.out.println("배열의 요소를 잘못액세스");
 		}catch(NullPointerException e) {
 			System.out.println("널입니다");
 		}catch(Exception e) {
 			System.out.println("오류발생");
+		}finally {
+			System.out.println("반드시 실행할 명령문 - 무조건 실행");
 		}
 		
 		
